54c86c77781eae88b70b42c0bcc9b2e535f8b8e5
m-stclair <mstclair@millionconcepts.com>
2025-01-11 18:42:00 -0500

    scratch infra

--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@ from setuptools import Extension, setup
 
 mpermute = Extension(
     "mpermute.mpermute_core",
-    sources=["mpermute/mpermute_core.c", "mpermute/unique.c"],
+    sources=[
+        "mpermute/mpermute_core.c", "mpermute/unique.c"
+    ],
     extra_compile_args=["-g"]
 )
 
